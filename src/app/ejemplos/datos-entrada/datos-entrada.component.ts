cb9acfad04a89dbb65eb22039454d7196292565e
CESAR RODRIGO RAMIREZ FLEITAS <cesarrodrigoramirez@gmail.com>
2024-08-23 16:20:25 -0400

    add formsModule

--- src/app/ejemplos/datos-entrada/datos-entrada.component.ts
+++ src/app/ejemplos/datos-entrada/datos-entrada.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./datos-entrada.component.css']
 })
 export class DatosEntradaComponent {
+  nombre: string;
+  archivo: File | undefined
+
+  constructor(){
+    this.nombre=""
+  }
+
+  observar(event: any){
+    console.log(event.target.value)
+  }
+
+  subirArchivo(event: any){
+    console.log(event.target.files[0]);
+    this.archivo = event.target.files[0]
+  }
+
+  guardar(){
+    console.log("Nombre: ", this.nombre);
+    console.log("Archivo: ",this.archivo?.name);
+    console.log("Tipo: ",this.archivo?.type);
+    console.log("Tamaño: ",this.archivo?.size);
+  }
 
 }
