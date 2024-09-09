80ed2269e4d59d038e666f9e9154e0a382e95cba
CESAR RODRIGO RAMIREZ FLEITAS <cesarrodrigoramirez@gmail.com>
2024-09-09 16:46:20 -0400

    add main-content

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { MainContentComponent } from './pages/main-content/main-content.component';
 
 const routes: Routes = [
-  //{path: '', redirectTo: '/home', pathMatch: 'full'},
-  {path: '', component: HomeComponent},
   { path: 'ejemplos',
-  loadChildren: ()=> import("./ejemplos/ejemplos.module").then(m => m.EjemplosModule)}
+    loadChildren: ()=> import("./ejemplos/ejemplos.module").then(m => m.EjemplosModule)
+  },
+  {path: '', component: HomeComponent,
+  children: [{path:'', component: MainContentComponent}]},
 ];
 
 @NgModule({
