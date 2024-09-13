7aa910fb3cab319511e114fadc064a434d956260
CESAR RODRIGO RAMIREZ FLEITAS <cesarrodrigoramirez@gmail.com>
2024-09-13 08:03:04 -0400

    add product-list maquetacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { MainContentComponent } from './pages/main-content/main-content.component';
+import { ProductListComponent } from './pages/products/product-list/product-list.component';
 
 const routes: Routes = [
   { path: 'ejemplos',
     loadChildren: ()=> import("./ejemplos/ejemplos.module").then(m => m.EjemplosModule)
   },
   {path: '', component: HomeComponent,
-  children: [{path:'', component: MainContentComponent}]},
+  children: [
+      {path:'', component: MainContentComponent},
+      {path:'products', component: ProductListComponent}
+    ]
+  },
 ];
 
 @NgModule({
